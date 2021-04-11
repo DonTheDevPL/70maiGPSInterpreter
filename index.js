935b23390e81516ebb9df80e5eebfb5db7f2b690
Michał <33964835+DonTheDevPL@users.noreply.github.com>
2021-04-11 20:25:26 +0200

    optymalizacja i fix

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 fs = require('fs');
 fs.readFile("C:\\GitHub\\70maiGPSInterpreter\\GPSData000001.txt"
-    , 'utf8', function (err,data) {
+    , 'utf8', function (err, data) {
+        class DaneGPS {
+            constructor(hour, speed, latitude, longitude) {
+                this.hour = hour;
+                this.speed = speed;
+                this.latitude = latitude;
+                this.longitude = longitude;
+            }
+        }
+
         if (err) {
             return console.log(err);
         }
         let lines = data.split('\n');
         let lines2 = [];
         let FinallData = [];
-        for (let x in lines){
+        for (let x in lines) {
             lines2.push(lines[x].split(','));
 
         }
@@ -18,36 +27,25 @@ fs.readFile("C:\\GitHub\\70maiGPSInterpreter\\GPSData000001.txt"
             temp1.push(element.split(' '));
         }
         let hours = [];
-        for (let x in temp1){
+        for (let x in temp1) {
             FinallData.push(temp1[x][0])
             hours.push(temp1[x][1]);
         }
-        let Gotowe = {};
-
-        for (let x in FinallData){
-            if (FinallData[x] in Gotowe){
-
-            }else{
-                let klucz2 = FinallData[x];
-                Gotowe[klucz2] = [];
-            }
-        }
-        for (let x in FinallData){
+        let DaneKompletne = {};
+        for (let x in FinallData) {
             let klucz2 = FinallData[x];
-            let temp = [];
-                    let temp2 = new Array();
-                    temp2.push(hours[x]);
-                    temp2.push(lines2[x][3]);
-                    temp2.push(lines2[x][4]);
-                    temp2.push(lines2[x][5]);
-                    temp.push(temp2);
+            if (klucz2 in DaneKompletne) {
 
+            } else {
+                DaneKompletne[klucz2] = [];
+            }
+            const temp= new DaneGPS(hours[x], lines2[x][3], lines2[x][4], lines2[x][5])
 
             console.log(klucz2);
-            let holder = Gotowe[klucz2];
+            let holder = DaneKompletne[klucz2];
             holder.push(temp);
-            //console.log(holder);
-            Gotowe[klucz2] = holder;
-            fs.writeFileSync("C:\\GitHub\\70maiGPSInterpreter\\output.json",JSON.stringify(Gotowe, null, 2) , 'utf-8');
+            DaneKompletne[klucz2] = holder;
+
         }
+        fs.writeFileSync("C:\\GitHub\\70maiGPSInterpreter\\output.json", JSON.stringify(DaneKompletne, null, 2), 'utf-8');
     });
